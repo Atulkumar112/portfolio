e1a8e52d78af585661a127b61f09bffd083349a4
Atul <atul.kumar112000@gmail.com>
2023-09-21 02:12:20 +0530

    second commit :)

--- src/portfolio.js
+++ src/portfolio.js
@@ -23,7 +23,7 @@ const greeting = {
   username: "Atul Kumar",
   title: "Hi all, I'm Atul",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web applications with JavaScript / Nestjs / Vuejs / Reactjs / Nodejs / SQL and some other cool libraries and frameworks."
+    "A passionate Full Stack Software Developer 🚀 having an experience of building Web applications with JAVA/ JavaScript / Nestjs / Vuejs / Reactjs / Nodejs / Spring Boot/ SQL and some other cool libraries and frameworks."
   ),
   resumeLink:
     "https://drive.google.com/file/d/1ZdMA8JEO3YpIZPk4phPXUeE39uYTf1Cr/view?usp=drive_link", // Set to empty to hide the button
@@ -283,50 +283,51 @@ const achievementSection = {
 
   achievementsCards: [
     {
-      title: "Google Code-In Finalist",
+      title: "Leetcode",
       subtitle:
-        "First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
-      image: require("./assets/images/codeInLogo.webp"),
+        "Solved over 500+ Java problems on LeetCode. Achieved a maximum contest rating of 1490. Maintained a daily coding challenge streak from July 12, 2022, to the present.",
+      image: require("./assets/images/leetcode.png"),
       imageAlt: "Google Code-In Logo",
       footerLink: [
         {
-          name: "Certification",
-          url: "https://drive.google.com/file/d/0B7kazrtMwm5dYkVvNjdNWjNybWJrbndFSHpNY2NFV1p4YmU0/view?usp=sharing"
-        },
-        {
-          name: "Award Letter",
-          url: "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing"
-        },
-        {
-          name: "Google Code-in Blog",
-          url: "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html"
+          name: "Visit",
+          url: "https://leetcode.com/atul9119"
         }
+        // {
+        //   name: "Award Letter",
+        //   url: "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing"
+        // },
+        // {
+        //   name: "Google Code-in Blog",
+        //   url: "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html"
+        // }
       ]
     },
     {
-      title: "Google Assistant Action",
+      title: "GeeksForGeeks",
       subtitle:
-        "Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.",
-      image: require("./assets/images/googleAssistantLogo.webp"),
+        "Solved over 400+ Java problems and Maintained the POTD streak from Augest 29, 2022 to the persent and got the gfg goodies (bag & shirt)",
+      image: require("./assets/images/gfg.png"),
       imageAlt: "Google Assistant Action Logo",
       footerLink: [
         {
-          name: "View Google Assistant Action",
-          url: "https://assistant.google.com/services/a/uid/000000100ee688ee?hl=en"
+          name: "Visit",
+          url: "https://auth.geeksforgeeks.org/user/atulkumar112000/practice"
         }
       ]
     },
 
     {
-      title: "PWA Web App Developer",
-      subtitle: "Completed Certifcation from SMIT for PWA Web App Development",
-      image: require("./assets/images/pwaLogo.webp"),
+      title: "Hacker Rank",
+      subtitle:
+        "Secure the 5 stars in JAVA, Problem Solving, SQL & Python and got certificates",
+      image: require("./assets/images/hackerrank.png"),
       imageAlt: "PWA Logo",
       footerLink: [
-        {name: "Certification", url: ""},
+        // {name: "Certification", url: ""},
         {
-          name: "Final Project",
-          url: "https://pakistan-olx-1.firebaseapp.com/"
+          name: "Visit",
+          url: "https://www.hackerrank.com/atul_kumar112000"
         }
       ]
     }
